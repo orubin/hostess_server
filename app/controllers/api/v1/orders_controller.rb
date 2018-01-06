d3d072b0be417eff1bcb0476ef8133731d70c762
Or Rubin <or.rubin@hotmail.com>
2018-01-06 22:35:01 +0200

    Add routes

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,6 +10,14 @@ class Api::V1::OrdersController < Api::V1::ApplicationController
     render json: result
   end
 
+  def create_order
+    render json: { code: 200, error: nil }
+  end
+
+  def create_order_waiting
+    render json: { code: 200, error: nil }
+  end
+
   def history
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, constraints: { format: :json } do
     namespace :v1 do
-      resources :orders
+      resources :orders do
+        collection do
+          put 'create_order' => :create_order
+          put 'create_order_waiting' => :create_order_waiting
+        end
+      end
+
       resources :users
       resources :restaurants
     end
