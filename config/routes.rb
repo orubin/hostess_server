c0c106384651689ea293189b196fcb7f22978100
Or Rubin <or.rubin@hotmail.com>
2018-01-06 22:43:01 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
       resources :restaurants do
         collection do
-          post 'special_requests' => :special_requests
+          get 'special_requests' => :special_requests
         end
       end
     end
