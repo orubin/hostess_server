92f7bedfc3d0ac511bf91ae918a5a7de6c0061cb
Or Rubin <or.rubin@hotmail.com>
2018-01-07 07:44:26 +0200

    Fixed users table

--- db/migrate/20171223163800_create_users.rb
+++ db/migrate/20171223163800_create_users.rb
@@ -3,9 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.text :description
-      t.token :auth_token
       t.string :password_digest
-
       t.timestamps null: false
     end
   end
